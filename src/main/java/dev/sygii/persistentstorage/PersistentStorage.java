7edb1b27ddf4402b4566dbfb87da18f910b4cd7d
Sygikal <xtin8873@gmail.com>
2025-09-05 19:57:15 -0700

    Initial commit

---
+++ src/main/java/dev/sygii/persistentstorage/PersistentStorage.java
@@ -0,0 +1,86 @@
+package dev.sygii.persistentstorage;
+
+import com.google.gson.*;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.JsonHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersistentStorage implements ModInitializer {
+	public static final String MOD_ID = "persistentstorage";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	public static final Path STORAGE_FILE = Path.of(FabricLoader.getInstance().getGameDir() + File.separator + "persistent_storage.json");
+
+	public static final Map<String, Object> STORAGE = new HashMap<>();
+
+	public static JsonObject JSON;
+	public static JsonArray TAGS;
+
+	@Override
+	public void onInitialize() {
+	}
+
+	public static void initStorage() {
+		try {
+			if (!Files.exists(PersistentStorage.STORAGE_FILE)) {
+				Files.write(STORAGE_FILE, GSON.toJson(new Storage()).getBytes());
+			}
+
+			BufferedReader inputStream = Files.newBufferedReader(STORAGE_FILE);
+			JSON = JsonParser.parseReader(inputStream).getAsJsonObject();
+			TAGS = JSON.get("tags").getAsJsonArray();
+		} catch (IOException ignored) {
+			ignored.printStackTrace();
+		}
+	}
+
+	public static boolean getTag(String tag) {
+		return TAGS.contains(GSON.toJsonTree(tag));
+	}
+
+	public static void putTag(String tag) {
+		if (!TAGS.contains(GSON.toJsonTree(tag))) {
+			TAGS.add(tag);
+			saveStorage();
+		}
+	}
+
+	public static JsonElement get(String key) {
+		return JSON.get(key);
+	}
+
+	public static void put(String key, Object value) {
+		JSON.add(key, GSON.toJsonTree(value));
+		saveStorage();
+	}
+
+	public static boolean getBoolean(String key) {
+		if (PersistentStorage.get(key) != null) {
+			return PersistentStorage.get(key).getAsBoolean();
+		}
+		return false;
+	}
+
+	public static void saveStorage() {
+		try {
+			Files.write(STORAGE_FILE, GSON.toJson(JSON).getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static class Storage {
+		public String[] tags = {};
+	}
+}
---
+++ src/main/java/dev/sygii/persistentstorage/mixin/PersistentStorageMixinPlugin.java
@@ -0,0 +1,52 @@
+package dev.sygii.persistentstorage.mixin;
+
+import dev.sygii.persistentstorage.PersistentStorage;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class PersistentStorageMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String s) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String s, String s1) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> set, Set<String> set1) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    static {
+        //This is the move that made lebron cry
+        //Legit shaves like 5 seconds off boot sequence
+        PersistentStorage.initStorage();
+    }
+}
